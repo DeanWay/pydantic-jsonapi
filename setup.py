8f84340e095b791945310de2a2b046d5241e7652
DeanWay <deanway93@gmail.com>
2019-09-13 16:26:28 -0400

    Bump version from 0.2.0 to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pydantic_jsonapi",
-    version="0.2.0",
+    version="0.2.1",
     author="Dean Way",
     description="an implementation of JSON:api using pydantic",
     long_description=long_description,
